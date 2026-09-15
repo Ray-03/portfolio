47a483cd1abf9574fd301eea9104324155dd9360
Ray-03 <rinaldy.richard.03@gmail.com>
2026-09-15 17:37:10 +0700

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.100.9", "127.0.0.1"],
+  async redirects() {
+    return [
+      { source: "/en", destination: "/", permanent: false },
+      { source: "/id", destination: "/", permanent: false },
+      { source: "/zh", destination: "/", permanent: false },
+      { source: "/de", destination: "/", permanent: false },
+      { source: "/en/:path*", destination: "/", permanent: false },
+      { source: "/id/:path*", destination: "/", permanent: false },
+      { source: "/zh/:path*", destination: "/", permanent: false },
+      { source: "/de/:path*", destination: "/", permanent: false },
+    ];
+  },
 };
 
 export default nextConfig;
